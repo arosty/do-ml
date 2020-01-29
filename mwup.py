11800b52beea3e056c4f6050ce8a6109b8b5d2b9
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-01-29 10:43:34 +0100

    coded mwup function

--- mwup.py
+++ mwup.py
@@ -1,13 +1,27 @@
 import numpy as np
 
-def mwup(eta):
-    pass
+def mwup(eta, M):
+    """
+    INPUT:
+    eta     -> float (in interval [0,0.5])
+    M       -> np.ndarray (2-dimensional, costs or gains for all rounds,
+               each row one round)
+    OUTPUT:
+    p       -> np.ndarray (1-dimensional, distribution)
+    """
+    T, n = M.shape
+    w = np.ones(n)
+    p = w / n
+    for m in M:
+        i = random_decision(p)      # for demonstrating purposes
+        w, p = update_rule(eta, w, m)
+    return p
 
 
 def random_decision(p):
     """
     INPUT:
-    p       -> np.array (distribution)
+    p       -> np.ndarray (1-dimensional, distribution)
     OUTPUT:
     i       -> int (randomly chosen decision)
     """
@@ -20,11 +34,11 @@ def update_rule(eta, w, m):
     """
     INPUT:
     eta     -> float (in interval [0,0.5])
-    w       -> np.array (weights)
-    m       -> np.array (same dimension as w, costs or gains)
+    w       -> np.ndarray (1-dimensional, weights)
+    m       -> np.ndarray (same dimension as w, costs or gains)
     OUTPUT:
-    w       -> np.array (updated weights)
-    p       -> np.array (new distribution)
+    w       -> np.ndarray (1-dimensional, updated weights)
+    p       -> np.ndarray (same dimension as w, new distribution)
     """
     w = (1 - eta * m) * w
     phi = w.sum()
