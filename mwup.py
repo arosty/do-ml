af919d452b006d94470f2f307df60c94db6b3be7
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-01-29 10:18:14 +0100

    coded random decision function

--- mwup.py
+++ mwup.py
@@ -3,17 +3,30 @@ import numpy as np
 def mwup(eta):
     pass
 
+
+def random_decision(p):
+    """
+    INPUT:
+    p       -> np.array (distribution)
+    OUTPUT:
+    i       -> int (randomly chosen decision)
+    """
+    n = p.size
+    i = np.random.choice(n, 1, p=p)[0]
+    return i
+
+
 def update_rule(eta, w, m):
     """
     INPUT:
     eta     -> float (in interval [0,0.5])
     w       -> np.array (weights)
     m       -> np.array (same dimension as w, costs or gains)
-    OUTPU:
+    OUTPUT:
     w       -> np.array (updated weights)
     p       -> np.array (new distribution)
     """
     w = (1 - eta * m) * w
-    phi = np.sum(w)
+    phi = w.sum()
     p = w / phi
     return w, p
